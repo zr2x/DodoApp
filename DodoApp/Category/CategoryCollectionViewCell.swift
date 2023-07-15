d8cf4812fcf376f87a541496608a98384cb42189
zr2x <master5212345@gmail.com>
2023-07-15 22:28:00 +0500

    update

---
+++ DodoApp/Category/CategoryCollectionViewCell.swift
@@ -0,0 +1,58 @@
+//
+//  CategoryCollectionViewCell.swift
+//  DodoApp
+//
+//  Created by Искандер Ситдиков on 14.07.2023.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    var containerView: UIView = {
+        let view = UIView()
+        view.heightAnchor.constraint(equalToConstant: 40).isActive = true
+    
+        return view
+    }()
+    
+    var categoryButton: UIButton = {
+        let categoryButton = UIButton()
+        categoryButton.backgroundColor = .lightGray
+        return categoryButton
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+        setupConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(categoryButton)
+    }
+    
+    private func setupConstraints() {
+        containerView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+        
+        categoryButton.snp.makeConstraints { make in
+//            make.top.bottom.equalTo(containerView)
+//            make.height.equalTo(35)
+//            make.width.equalTo(60)
+            make.edges.equalTo(containerView)
+        }
+        
+        func update(_ category: Category) {
+            categoryButton.setTitle(category.name, for: .normal)
+        }
+    }
+}
